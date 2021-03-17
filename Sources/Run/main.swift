0d35b3423dbb5dba6cf0110fda8bd05883f464c8
zhenghuiwin <bluegene.hao@gmail.com>
2021-03-17 10:32:51 +0800

    2021-3-18 10_32.

--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -5,20 +5,20 @@ import Foundation
 
 print("Hello, world!")
 
-let count = 100000
-let progressBar = ProgressBar(count: count)
-for _ in 1 ... count {
-    Thread.sleep(forTimeInterval: 0.000001)
-    progressBar.add(progress: 1)
-}
-
-//let timeUtils = TimeUtils()
-//let extor = DataExtractor()
-//
-//do {
-////    try timeUtils.buildTargetTimes()
-//    try extor.extract()
-//    print("buildTargetTimes completed.")
-//} catch let e {
-//    print("[Error: \(e)]")
+//let count = 100000
+//let progressBar = ProgressBar(count: count)
+//for _ in 1 ... count {
+//    Thread.sleep(forTimeInterval: 0.000001)
+//    progressBar.add(progress: 1)
 //}
+
+let timeUtils = TimeUtils()
+let extor = DataExtractor()
+
+do {
+//    try timeUtils.buildTargetTimes()
+    try extor.extract()
+    print("\n buildTargetTimes completed.")
+} catch let e {
+    print("[Error: \(e)]")
+}
